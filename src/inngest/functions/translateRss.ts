6b75763f6decb9d02d397e1f5038428a9c374836
hestudy <1467602180@qq.com>
2025-03-04 23:35:05 +0800

    ✨ feat(translateRss.ts)：增强RSS翻译功能，添加翻译配置和保存逻辑 ♻️ refactor(translateRss.ts)：简化数据库查询结构，优化代码可读性

--- src/inngest/functions/translateRss.ts
+++ src/inngest/functions/translateRss.ts
@@ -1,6 +1,8 @@
-import { isNull } from "drizzle-orm";
+import { eq, isNull } from "drizzle-orm";
+import type Parser from "rss-parser";
 import { db } from "~/server/db";
-import { newRssTranslateDataItem } from "~/server/db/schema";
+import { newRssTranslateDataItem, rssTranslate } from "~/server/db/schema";
+import { translate } from "~/utils/translate";
 import { inngest } from "../client";
 
 export const translateRss = inngest.createFunction(
@@ -17,19 +19,69 @@ export const translateRss = inngest.createFunction(
         return await db.query.newRssTranslateDataItem.findMany({
           where: isNull(newRssTranslateDataItem.data),
           with: {
-            newRssTranslateData: {
-              with: {
-                rssTranslate: {
-                  with: {
-                    translateOriginData: true,
-                    rssOriginData: true,
-                  },
-                },
-              },
-            },
+            newRssTranslateData: true,
           },
         });
       },
     );
+
+    for (const item of noTranslateRssItemList) {
+      const origin = item.origin as Parser.Item;
+      const rssTranslateRecord = await step.run(
+        `get rss translate config: ${item.link}`,
+        async () => {
+          return await db.query.rssTranslate.findFirst({
+            where: eq(rssTranslate.id, item.newRssTranslateData.rssTranslateId),
+            with: {
+              rssOriginData: true,
+              translateOriginData: true,
+              translatePromptData: true,
+            },
+          });
+        },
+      );
+
+      if (rssTranslateRecord) {
+        const title = await step.run(
+          `translate rss title: ${origin.title}`,
+          async () => {
+            return await translate({
+              apiKey: rssTranslateRecord.translateOriginData.apiKey ?? "",
+              language: rssTranslateRecord.language ?? "",
+              model: rssTranslateRecord.translateOriginData.model ?? "",
+              prompt: rssTranslateRecord.translatePromptData.prompt ?? "",
+              baseUrl: rssTranslateRecord.translateOriginData.baseUrl ?? "",
+              content: origin.title,
+            });
+          },
+        );
+
+        const content = await step.run(
+          `translate rss content: ${origin.content}`,
+          async () => {
+            return await translate({
+              apiKey: rssTranslateRecord.translateOriginData.apiKey ?? "",
+              language: rssTranslateRecord.language ?? "",
+              model: rssTranslateRecord.translateOriginData.model ?? "",
+              prompt: rssTranslateRecord.translatePromptData.prompt ?? "",
+              baseUrl: rssTranslateRecord.translateOriginData.baseUrl ?? "",
+              content: origin.content,
+            });
+          },
+        );
+
+        await step.run(`save translate rss: ${item.link}`, async () => {
+          await db
+            .update(newRssTranslateDataItem)
+            .set({
+              data: {
+                title,
+                content,
+              },
+            })
+            .where(eq(newRssTranslateDataItem.id, item.id));
+        });
+      }
+    }
   },
 );
